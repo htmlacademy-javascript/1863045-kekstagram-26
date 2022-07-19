57f7a8ec5374d2ddec4ba7a453adc6d2f9bce4f5
alex_penzin <pepa88003553555@mail.ru>
2022-07-19 04:33:10 +0300

    Создал модуль по отрисовки фотографий

---
+++ js/create-picture.js
@@ -0,0 +1,18 @@
+import {getPhotosArray} from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+
+const photosArray = getPhotosArray();
+
+const simularPhotos = document.createDocumentFragment();
+
+photosArray.forEach((photo) => {
+  const photoElement = pictureTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments').textContent = photo.length;
+  simularPhotos.append(photoElement);
+});
+
+picturesContainer.append(simularPhotos);
